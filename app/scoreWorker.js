e6c545a6739f5ef51dc98b405a4675f3387b0f06
huanhulan <huanhulan@163.com>
2017-03-16 18:58:21 +0800

    fix(index): replace positionInPath to collision detection in worker

---
+++ app/scoreWorker.js
@@ -0,0 +1,23 @@
+onmessage = function(e) {
+    var d = e.data;
+    var pipes = d.pipes;
+    var x = d.x;
+    var y = d.y;
+    var score = d.score;
+    var lastId = d.lastId;
+    var res = {};
+    for (var i = pipes.length - 1; i >= 0; i--) {
+        var pipe = pipes[i];
+
+        if (x >= pipe.x && x <= (pipe.x + pipe.imgW) && lastId !== pipe.id) {
+            if ((y >= pipe.upY) && (y <= pipe.upY + pipe.imgH) || (y >= pipe.downY) && (y <= pipe.downY + pipe.imgH)) {
+                res.gameOver = true;
+                return postMessage(res);
+            } else {
+                res.score = score + 1;
+                res.lastId = pipe.id;
+                return postMessage(res);
+            }
+        }
+    }
+};
